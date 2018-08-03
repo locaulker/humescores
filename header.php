0193d7982d981ff715ef28f0a337210bbfc7531b
Lawrence Caulker <locaulker@gmail.com>
2018-08-03 18:06:02 -0400

    section 2: build the header

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
  */
 
 ?>
-<!doctype html>
+<!Doctype html>
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
@@ -24,24 +24,35 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'humescores' ); ?></a>
 
+	<!-- Custom Header Image -->
+	<?php if (get_header_image() && is_front_page()) : ?>
+		<figure class="header-image">
+			<?php the_header_image_tag(); ?>
+		</figure><!-- .header-image -->
+	<?php endif; ?>
+
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<?php the_custom_logo(); ?>
+			
+			<div class="site-branding__text">
 				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
+				if ( is_front_page() && is_home() ) :
+					?>
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<?php
+				else :
+					?>
+					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+					<?php
+				endif;
+			
 			$humescores_description = get_bloginfo( 'description', 'display' );
 			if ( $humescores_description || is_customize_preview() ) :
 				?>
 				<p class="site-description"><?php echo $humescores_description; /* WPCS: xss ok. */ ?></p>
 			<?php endif; ?>
+			</div><!-- .site-branding__text -->
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
